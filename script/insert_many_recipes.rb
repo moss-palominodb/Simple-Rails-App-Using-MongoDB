ff03dc582dd16e8e5b3058b74c5b44507116cae8
Moss Gross <moss@palominodb.com>
2012-02-23 14:21:17 -0500

    New recipe loader

--- script/insert_many_recipes.rb
+++ script/insert_many_recipes.rb
@@ -26,7 +26,7 @@ def get_amount
   ['1', '1/2', '3', '4.5'].sample
 end
 
-(1..2000).each do |num|
+(1..20000).each do |num|
   r = Recipe.new(:name => get_name + ' ' + num.to_s,
                  :ingredients => get_ingredients)
   puts r.inspect
